385f6f34d6b9d682c4ac404021d9e2c26ac96d2d
liliia <llanovsk@gmail.com>
2022-07-01 14:28:54 +0200

    Added a Fraud customer check

--- customer/src/main/java/com/example/customer/service/CustomerService.java
+++ customer/src/main/java/com/example/customer/service/CustomerService.java
@@ -2,10 +2,12 @@ package com.example.customer.service;
 
 import com.example.customer.model.Customer;
 import com.example.customer.model.CustomerRegistrationRequest;
+import com.example.customer.model.FraudCheckResponse;
 import com.example.customer.repository.CustomerRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -13,8 +15,8 @@ import java.util.List;
 @AllArgsConstructor
 public class CustomerService {
 
-    @Autowired
     private final CustomerRepository repository;
+    private final RestTemplate restTemplate;
 
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
@@ -22,7 +24,13 @@ public class CustomerService {
                 .lastName(request.getLastName())
                 .email(request.getEmail())
                 .build();
-        repository.save(customer);
+        repository.saveAndFlush(customer);
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http://localhost:9001/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class, customer.getId());
+        if (fraudCheckResponse != null && fraudCheckResponse.isFraudster()) {
+            throw new IllegalStateException("Fraudster");
+        }
     }
 
     public List<Customer> getList() {
--- fraud/src/main/java/com/example/fraud/api/FraudCheckController.java
+++ fraud/src/main/java/com/example/fraud/api/FraudCheckController.java
@@ -3,8 +3,10 @@ package com.example.fraud.api;
 import com.example.fraud.model.FraudCheckResponse;
 import com.example.fraud.service.FraudCheckService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @AllArgsConstructor
@@ -15,6 +17,7 @@ public class FraudCheckController {
     @GetMapping(path = "{customerId}")
     public FraudCheckResponse checkFraudCheckHistory(@PathVariable Long customerId) {
         boolean isFraudulentCustomer = service.isFraudulentCustomer(customerId);
+        log.info("Fraud check request for customer {}", customerId);
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 }
